4b0113b1ac769402c3b360d822bfc63e0f56bf43
Elizaveta Kritskaya <post.kri@gmail.com>
2023-09-17 17:14:56 +0300

    add useState

---
+++ src/useState/UseState.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+const UseState : React.FC = () => {
+
+    let [a, setA] = useState(1)
+
+    const onClickHandler = () => {
+        setA(++a);
+        console.log(a)
+    }
+
+    const onClickHandler1 = () => {
+        setA(a=0);
+        console.log(a)
+    }
+
+
+    return (
+        <div className={"App"}>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler1}>0</button>
+        </div>
+    );
+};
+
+export default UseState;
